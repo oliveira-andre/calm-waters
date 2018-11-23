4bb19933eb0eb914e1cb16e2f67b6a43b62ed265
Andre Oliveira <desmonplays@gmail.com>
2018-11-23 19:39:05 -0400

    fixigin the dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,9 @@
 class DashboardsController < ApplicationController
   def index
     @tanks = Tank.all
+    @biomassa = 0
+    Specie.all.each do |specie|
+      @biomassa += specie.weight
+    end 
   end
 end
