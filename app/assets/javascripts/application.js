1f1deef559fc9071fa4345f10960796119758f37
Andre Oliveira <desmonplays@gmail.com>
2018-11-26 00:29:19 -0400

    adding the notification to project :heavy_plus_sign:

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -2,7 +2,7 @@
 //= require activestorage
 //= require turbolinks
 //= require jquery.mask
-
+var notification_class = document.getElementsByClassName('notification');
 $(document).on("turbolinks:load", function () {
     $(".date").datepicker({
         format: "dd/mm/yyyy",
@@ -19,4 +19,41 @@ $(document).on("turbolinks:load", function () {
         $(".cpf").unmask();
     });
 
+    //notification
+    for (var i = 0 ; i < notification_class.length; i++) {
+        notification_class[i].addEventListener("click", notificationDismiss);
+        return false
+    };
 });
+
+
+//notification
+function notification(type, text){
+    for (i = 0, notification_class.length; i < notification_class.length; i++) {
+        if (type == 'success') {
+            notification_class[i].style.backgroundColor = '#34bfa3';
+        } else if (type == 'error') {
+            notification_class[i].style.backgroundColor = '#f4516c';
+        } else if (type == 'warning') {
+            notification_class[i].style.backgroundColor = '#ffb822';
+        } else if (type == 'info') {
+            notification_class[i].style.backgroundColor = '#36a3f7';
+        }
+        notification_class[i].style.visibility = 'visible';
+        notification_class[i].innerHTML = text;
+        notification_class[i].style.animation = 'fadein 0.5s, fadeout 0.5s 2.5s';
+        setTimeout(function () {
+            notificationDismiss();
+        }, 2700);
+
+    }
+
+}
+
+function notificationDismiss(){
+    for (i = 0, notification_class.length; i < notification_class.length; i++) {
+        notification_class[i].style.visibility = 'hidden';
+        notification_class[i].style.animation = '';
+    }
+
+}
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def flash_message
+    notification = ''
+    [:success, :info, :warning, :error].each do |type|
+      if flash[type]
+        notification += "<script>notification('#{type}', '#{flash[type]}');</script>"
+      end
+    end
+    notification.html_safe
+  end
 end
