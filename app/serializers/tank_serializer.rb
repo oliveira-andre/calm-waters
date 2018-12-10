e11eb61e190334e990effd6cb0a766f6b70a0cd1
André Oliveira <andreoliveirar2d2@gmail.com>
2018-12-10 17:05:24 -0400

    adding attributes to api :heavy_plus_sign:

--- app/serializers/tank_serializer.rb
+++ app/serializers/tank_serializer.rb
@@ -1,9 +1,9 @@
 class TankSerializer < ActiveModel::Serializer
-  attributes :id, :acidity, :temperature, :oxigen, :ammonia, :ph
+  attributes :id, :acidity, :temperature, :oxigen, :ammonia, :ph, :depopulation_date, :final_quantity
 
   def diary_data
     tank = Tank.find(object[:id])
-    tank.update(acidity: object[:acidity], temperature: object[:temperature], oxigen: object[:oxigen], ammonia: object[:ammonia], ph: object[:ph])
+    tank.update(acidity: object[:acidity], temperature: object[:temperature], oxigen: object[:oxigen], ammonia: object[:ammonia], ph: object[:ph], depopulation_date: object[:depopulation_date], final_quantity: object[:final_quantity])
     tank
   end
 
